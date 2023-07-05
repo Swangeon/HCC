1bfac5af56c03b16cb70567d4bcc6bd891423463
Sean Brady <60947265+Swangeon@users.noreply.github.com>
2023-07-04 23:48:59 -0500

    Update icmp_reply.py

--- tests/icmp_reply.py
+++ tests/icmp_reply.py
@@ -1,16 +1,19 @@
+from pypacker.layer12 import ethernet
 from pypacker.layer3 import ip
 from pypacker.layer3 import icmp
 
 with open("/dev/misc/tun_driver", "rb+") as file:
   try:
-    file.read()
+    file.read() # For some reason when open is called it skips the semaphore?
   except OSError:
     print("Reading...")
-    data = file.read(1500) # MTU
+    data = file.read(84)
     print(f"Read Data -> {data}\nResponding...")
-    reply = ip.IP(data)
-    reply.higher_layer.type = icmp.ICMP_ECHO_REPLY
-    # print("%s" % reply)
+    reply = ethernet.Ethernet(src_s="00:00:00:00:00:00", dst_s="00:00:00:00:00:00")
+    temp = ip.IP(data)
+    temp.higher_layer.type = icmp.ICMP_ECHO_REPLY
+    reply += temp
+    print("%s" % reply)
     print("Writing...")
     file.write(reply.bin())
     print("Done")
