9c0ba363c67ff9d07f7551642259f84c7308f63f
Sean Brady <60947265+Swangeon@users.noreply.github.com>
2023-07-21 22:10:28 -0500

    Update udp_client.py

--- tests/udp_client.py
+++ tests/udp_client.py
@@ -2,7 +2,7 @@ from pypacker.layer3 import ip
 from pypacker.layer4 import udp
 
 with open("/dev/misc/tun_driver", "rb+") as file:
-  echo = b"CLIENTECHO"
+	echo = b"CLIENTECHO"
 	print("Making Packet...")
 	ip_header = ip.IP(p=ip.IP_PROTO_UDP, src_s="1.1.1.1", dst_s="1.1.1.1")
 	udp_packet = udp.UDP(sport=3334, dport=3333, body_bytes=echo)
@@ -11,10 +11,7 @@ with open("/dev/misc/tun_driver", "rb+") as file:
 	print("Sending...")
 	file.write(full_packet.bin())
 	print(f"Reading Data...")
-	try:
-		file.read()
-	except OSError:
-		received = file.read(51)
-		print(f"Data -> {received}")
-		#received = ip.IP(received)
-		#print("Packet %s" % received)
+	received = file.read(51)
+	#print(f"Data -> {received}")
+	received = ip.IP(received)
+	print("Packet %s" % received)
