dbdff7bd895d752a34db08605c9877e3c47dfe46
Sean Brady <60947265+Swangeon@users.noreply.github.com>
2023-07-22 17:46:26 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -11,7 +11,6 @@
 #include <net_buffer.h>
 
 #include <fcntl.h>
-// #include <fsproto.h>
 #include <net/if.h>
 #include <net/if_types.h>
 #include <net/if_media.h>
@@ -22,15 +21,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
-#define TUN_DRIVER_NAME "misc/tun_driver"
+#define TUN_DRIVER_NAME "tun0"
 
 #define NET_TUN_MODULE_NAME "network/devices/tun/v1"
 
-const char * device_names[] = {TUN_DRIVER_NAME, "misc/tun_interface", NULL};
+const char *device_names[] = {TUN_DRIVER_NAME, "misc/tun_interface", NULL};
 
 int32 api_version = B_CUR_DRIVER_API_VERSION;
 
+static net_buffer *create_filled_buffer(uint8 *data, size_t bytes);
+static status_t get_packet_data(void *data, size_t *numbytes, net_buffer *buffer);
+static status_t retreive_packet(BufferQueue *queueToUse, void *data, size_t *numbytes);
 
 status_t tun_open(const char *name, uint32 flags, void **cookie);
 status_t tun_close(void *cookie);
@@ -41,25 +42,146 @@ status_t tun_write(void *cookie, off_t position, const void *data, size_t *numby
 status_t tun_readv(void *cookie, off_t position, const iovec *vec, size_t count, size_t *numBytes);
 status_t tun_writev(void *cookie, off_t position, const iovec *vec, size_t count, size_t *numBytes);
 
-
 device_hooks tun_hooks = {
-        tun_open,
-        tun_close,
-        tun_free,
-        tun_ioctl,
-        tun_read,
-        tun_write,
-        NULL,
-        NULL,
-        tun_readv,
-        tun_writev
-};
+    tun_open,
+    tun_close,
+    tun_free,
+    tun_ioctl,
+    tun_read,
+    tun_write,
+    NULL,
+    NULL,
+    tun_readv,
+    tun_writev};
 
-BufferQueue appQ(3000); // 3000 for now
-BufferQueue interfaceQ(3000); // 3000 for now
-sem_id intSem;
-sem_id appSem;
-struct net_buffer_module_info* gBufferModule;
+/**
+ * @brief Represents the tun_struct data structure used for tun driver management.
+ *
+ * The `tun_struct` struct holds various semaphores and BufferQueue pointers
+ * that are used for tun driver management and communication.
+ *
+ * @var selfSendSem  The semaphore ID used for self-send operations.
+ * @var selfRecvSem  The semaphore ID used for self-receive operations.
+ * @var sendSem      A pointer to the semaphore ID used for sending data to the interface.
+ * @var recvSem      A pointer to the semaphore ID used for receiving data from the interface.
+ * @var sendQueue    A pointer to the BufferQueue used for sending data to the interface.
+ * @var recvQueue    A pointer to the BufferQueue used for receiving data from the interface.
+ */
+typedef struct tun_struct
+{
+    sem_id selfSendSem;
+    sem_id selfRecvSem;
+    sem_id *sendSem;
+    sem_id *recvSem;
+    BufferQueue *sendQueue;
+    BufferQueue *recvQueue;
+} tun_struct;
+
+BufferQueue gAppQueue(30000);
+BufferQueue gIntQueue(30000);
+sem_id gIntReadSem;
+sem_id gAppReadSem;
+sem_id gIntWriteSem;
+sem_id gAppWriteSem;
+struct net_buffer_module_info *gBufferModule;
+
+/**
+ * @brief Creates a new net_buffer and fills it with the specified data.
+ *
+ * This function creates a new net_buffer using the gBufferModule and fills it with the
+ * data provided. The net_buffer must be later freed by calling gBufferModule->free().
+ *
+ * @param data  Pointer to the byte stream data to be copied into the net_buffer.
+ * @param bytes Number of bytes to copy from the data pointer.
+ * @return A pointer to the newly created net_buffer if successful, NULL otherwise.
+ */
+static net_buffer *
+create_filled_buffer(uint8 *data, size_t bytes)
+{
+    net_buffer *buffer = gBufferModule->create(256);
+    if (buffer == NULL)
+    {
+        return NULL;
+    }
+
+    status_t status = gBufferModule->append(buffer, data, bytes);
+    if (status != B_OK)
+    {
+        gBufferModule->free(buffer);
+        return NULL;
+    }
+
+    return buffer;
+}
+
+/**
+ * @brief Reads data from a net_buffer into a memory buffer.
+ *
+ * This function reads data from the specified net_buffer and stores it in the
+ * provided memory buffer. The number of bytes to read is determined by the value
+ * pointed to by numbytes.
+ *
+ * @param data     A pointer to the memory buffer where the data will be stored.
+ * @param numbytes A pointer to a size_t variable specifying the number of bytes
+ *                 to read from the net_buffer. Upon return, this variable will
+ *                 contain the actual number of bytes read.
+ * @param buffer   A pointer to the net_buffer from which to read the data.
+ * @return status_t The status of the operation. B_OK if successful, an error code
+ *                  otherwise.
+ */
+static status_t
+get_packet_data(void *data, size_t *numbytes, net_buffer *buffer)
+{
+    status_t status;
+    status = gBufferModule->read(buffer, 0, data, *numbytes);
+    if (status != B_OK)
+    {
+        dprintf("Failed reading data\n");
+        gBufferModule->free(buffer);
+        return status;
+    }
+    return B_OK;
+}
+
+/**
+ * @brief Retrieve a packet from the specified BufferQueue.
+ *
+ * This function retrieves a packet from the specified BufferQueue and populates the data
+ * buffer with the packet's content. The numbytes parameter will be updated with the size
+ * of the retrieved packet data.
+ *
+ * @param queueToUse Pointer to the BufferQueue to retrieve the packet from.
+ * @param data       Pointer to the buffer that will hold the retrieved packet data.
+ * @param numbytes   Pointer to the size of the data buffer. Will be updated with the actual
+ *                   size of the retrieved packet data.
+ *
+ * @return status_t Returns B_OK if the packet was successfully retrieved and the data
+ *                  populated. Returns an appropriate error code otherwise.
+ */
+static status_t
+retreive_packet(BufferQueue *queueToUse, void *data, size_t *numbytes)
+{
+    net_buffer *buffer;
+    status_t status = B_OK;
+
+    if (queueToUse->Used())
+    {
+        status = queueToUse->Get(*numbytes, true, &buffer);
+        if (status != B_OK)
+        {
+            return status;
+        }
+        *numbytes = buffer->size;
+        status = get_packet_data(data, numbytes, buffer);
+        if (status != B_OK)
+        {
+            dprintf("Couldn't get packet data\n");
+            return B_ERROR;
+        }
+        status = B_OK;
+    }
+    return status;
+}
 
 status_t
 init_hardware(void)
@@ -68,61 +190,102 @@ init_hardware(void)
     return B_OK;
 }
 
-
 status_t
 init_driver(void)
 {
     /* Init driver */
-    dprintf("tun:init_driver() at /dev/misc\n");
     status_t status = get_module(NET_BUFFER_MODULE_NAME, (module_info **)&gBufferModule);
-    if (status != B_OK){
+    if (status != B_OK)
+    {
         dprintf("Getting BufferModule failed\n");
         return status;
     }
     return B_OK;
 }
 
-
-void
-uninit_driver(void)
+void uninit_driver(void)
 {
+    put_module(NET_BUFFER_MODULE_NAME);
+    delete_sem(gIntReadSem);
+    delete_sem(gAppReadSem);
+    delete_sem(gIntWriteSem);
+    delete_sem(gAppWriteSem);
     dprintf("tun:uninit_driver()\n");
 }
 
-
+/**
+ * @brief Opens the tun driver interface and sets up the driver for the interface.
+ *
+ * This function opens the tun driver interface, initializes necessary resources,
+ * and sets up the driver for the interface operation.
+ *
+ * @param name   The name of the tun driver interface.
+ * @param flags  Flags for the driver interface (if applicable).
+ * @param cookie A pointer to a void pointer that will hold the tun_struct data
+ *              after the function call.
+ *
+ * @return       Returns B_OK on success, or an appropriate error code on failure.
+ */
 status_t
 tun_open(const char *name, uint32 flags, void **cookie)
 {
-    /* Make interface here */
-    dprintf("tun:open_driver with name %s and flags %u\n", name, flags);
-    if (strncmp(name, "misc/tun_interface", 19) == 0) { // 2 is the flag that gets passed by a tun interface
-        *cookie = (void*)"tun";
-        dprintf("Marked as interface for driver\n");
-        if ((intSem = create_sem(1, "tun_notify_int")) < B_NO_ERROR) {
-            put_module(NET_BUFFER_MODULE_NAME);
+    /* Setup driver for interface here */
+    tun_struct *tun = new tun_struct();
+    if (strncmp(name, "misc/tun_interface", 18) == 0)
+    {
+        gIntReadSem = create_sem(0, "tun_notify_read");
+        if (gIntReadSem < B_NO_ERROR)
+        {
             return B_ERROR;
         }
-    } else { 
-        *cookie = (void*)"app";
-        dprintf("Marked as application for driver\n");
-        if ((appSem = create_sem(1, "tun_notify_app")) < B_NO_ERROR) {
-            put_module(NET_BUFFER_MODULE_NAME);
+        gIntWriteSem = create_sem(1, "tun_notify_write");
+        if (gIntWriteSem < B_NO_ERROR)
+        {
             return B_ERROR;
         }
-    } 
+        tun->selfSendSem = gIntWriteSem;
+        tun->selfRecvSem = gIntReadSem;
+        tun->sendSem = &gAppWriteSem;
+        tun->recvSem = &gAppReadSem;
+        tun->sendQueue = &gAppQueue;
+        tun->recvQueue = &gIntQueue;
+        *cookie = static_cast<void *>(tun);
+    }
+    else
+    {
+        gAppReadSem = create_sem(0, "app_notify_read");
+        if (gAppReadSem < B_NO_ERROR)
+        {
+            return B_ERROR;
+        }
+        gAppWriteSem = create_sem(1, "app_notify_write");
+        if (gAppWriteSem < B_NO_ERROR)
+        {
+            return B_ERROR;
+        }
+        tun->selfSendSem = gAppWriteSem;
+        tun->selfRecvSem = gAppReadSem;
+        tun->sendSem = &gIntWriteSem;
+        tun->recvSem = &gIntReadSem;
+        tun->sendQueue = &gIntQueue;
+        tun->recvQueue = &gAppQueue;
+        *cookie = static_cast<void *>(tun);
+    }
+
     return B_OK;
 }
-
 
 status_t
 tun_close(void *cookie)
 {
     /* Close interface here */
     dprintf("tun:close_driver()\n");
-    // (void)cookie;
+    tun_struct *tun = static_cast<tun_struct *>(cookie);
+    delete tun;
+    tun = NULL;
+    cookie = NULL;
     return B_OK;
 }
-
 
 status_t
 tun_free(void *cookie)
@@ -130,189 +293,90 @@ tun_free(void *cookie)
     return B_OK;
 }
 
-
 status_t
 tun_ioctl(void *cookie, uint32 op, void *data, size_t len)
 {
-// 	switch (op) {
-// 	case B_SET_NONBLOCKING_IO:
-// 		cookie->blocking_io = false;
-// 		return B_OK;
-// 	case B_SET_BLOCKING_IO:
-// 		cookie->blocking_io = true;
-// 		return B_OK;
-// 	case TUNSETNOCSUM:
-// 		return B_OK;//EOPNOTSUPP;
-// 	case TUNSETDEBUG:
-// 		return B_OK;//EOPNOTSUPP;
-// 	case TUNSETIFF:
-// 		if (data == NULL)
-// 			return EINVAL;
-// 		ifr = (ifreq_t *)data;
-
-// 		iface = gIfaceModule->tun_reuse_or_create(ifr, cookie);
-// 		if (iface != NULL) {
-// 			dprintf("tun: new tunnel created: %s, flags: 0x%08lx\n", ifr->ifr_name, iface->flags);
-// 			return B_OK;
-// 		} else
-// 			dprintf("tun: can't allocate a new tunnel!\n");
-// 		break;
-
-// 	case SIOCGIFHWADDR:
-// 		if (data == NULL)
-// 			return EINVAL;
-// 		ifr = (ifreq_t *)data;
-// 		if (iface == NULL)
-// 			return EINVAL;
-// 		if (strncmp(ifr->ifr_name, iface->ifn->if_name, IFNAMSIZ) != 0)
-// 			return EINVAL;
-// 		memcpy(ifr->ifr_hwaddr, iface->fakemac.octet, 6);
-// 		return B_OK;
-// 	case SIOCSIFHWADDR:
-// 		if (data == NULL)
-// 			return EINVAL;
-// 		ifr = (ifreq_t *)data;
-// 		if (iface == NULL)
-// 			return EINVAL;
-// 		if (strncmp(ifr->ifr_name, iface->ifn->if_name, IFNAMSIZ) != 0)
-// 			return EINVAL;
-// 		memcpy(iface->fakemac.octet, ifr->ifr_hwaddr, 6);
-// 		return B_OK;
-
-// 	}
-// 	return B_ERROR;
+    /* IOCTL for driver */
     return B_OK;
 }
 
-
-status_t
-get_packet_data(void *data, size_t *numbytes, net_buffer* buffer)
-{
-    status_t status;
-    // set data to the uint8* byte stream?
-	status = gBufferModule->read(buffer, 0, data, *numbytes);
-	if (status != B_OK) {
-		dprintf("Failed reading data\n");
-        gBufferModule->free(buffer);
-        return status;
-    }
-    return B_OK;
-}
-
-
+/**
+ * @brief Reads data from the TUN device.
+ *
+ * This function reads data from the TUN device represented by the given `cookie` which
+ * is casted to a tun_struct. It acquires the appropriate semaphores, retrieves the
+ * packet from the receive queue, and releases the opposite send semaphore once the read
+ * operation is complete.
+ *
+ * @param cookie   A pointer to a `tun_struct` representing the TUN interface/application.
+ * @param position The position from where to read data (not used in this function).
+ * @param data     A pointer to a buffer where the read data will be stored.
+ * @param numbytes On input, the number of bytes to read; on output, the number of bytes read
+ *                 (see retreive_packet).
+ *
+ * @return A status code indicating the success or failure of the read operation.
+ *         B_OK if successful, an error code otherwise.
+ */
 status_t
 tun_read(void *cookie, off_t position, void *data, size_t *numbytes)
 {
-    // Read data from driver 
+    tun_struct *tun = static_cast<tun_struct *>(cookie);
     status_t status;
-    net_buffer* buffer;
-    if (strncmp((char*)cookie, "tun", 3) == 0) {
-        dprintf("Acquiring Interface Semaphore\n");
-        status = acquire_sem_etc(intSem, 1, B_CAN_INTERRUPT, 0);
-        if (status < B_OK) {
-            *numbytes = 0;
-            return status;
-        }
-    } else {
-        dprintf("Acquiring Application Semaphore\n");
-        status = acquire_sem_etc(appSem, 1, B_CAN_INTERRUPT, 0);
-        if (status < B_OK) {
-            *numbytes = 0;
-            return status;
-        }
+    status = acquire_sem_etc(tun->selfRecvSem, 1, B_CAN_INTERRUPT, 0);
+    if (status < B_OK)
+    {
+        dprintf("Could not acquire receiving sem\n");
+        *numbytes = 0;
+        return status;
     }
-    dprintf("TUN: Reading %li bytes of data\n", *numbytes);
-    if ((strcmp((char*)cookie, "app") == 0) && (appQ.Used() != 0)) {
-        dprintf("TUN: APP Reading %li bytes of data\n", *numbytes);
-        // dprintf("%li bytes available in appQ\n", appQ.Used());
-        status = appQ.Get(*numbytes, true, &buffer);
-        if (status != B_OK) {
-            dprintf("getting packet failed: %s\n", strerror(status));
-            return status;
-        }
-        *numbytes = buffer->size;
-        void* temp = malloc(*numbytes);
-        if (temp == NULL) {
-            dprintf("Malloc failed, not enough memory\n");
-            return B_ERROR;
-        }
-        if (get_packet_data(temp, numbytes, buffer) != B_OK) {
-            dprintf("Could not get byte stream\n");
-            return B_ERROR;
-        }
-        memcpy(data, temp, *numbytes);
-        free(temp);
-        temp = NULL;
-    } else if ((strcmp((char*)cookie, "tun") == 0) && (interfaceQ.Used() != 0)) {
-        dprintf("TUN: INTERFACE Reading %li bytes of data\n", *numbytes);
-        // dprintf("%li bytes available in interfaceQ\n", interfaceQ.Used());
-        status = interfaceQ.Get(*numbytes, true, &buffer);
-        if (status != B_OK) {
-            dprintf("getting packet failed: %s\n", strerror(status));
-            return status;
-        }
-        *numbytes = buffer->size;
-        void* temp = malloc(*numbytes);
-        if (temp == NULL) {
-            dprintf("Malloc failed, not enough memory\n");
-            return B_ERROR;
-        }
-        if (get_packet_data(temp, numbytes, buffer) != B_OK) {
-            dprintf("Could not get byte stream\n");
-            return B_ERROR;
-        }
-        memcpy(data, temp, *numbytes);
-        free(temp);
-        temp = NULL;
-    } else {
-        dprintf("No Data\n");
-        data = NULL;
-        return B_ERROR;
+    status = retreive_packet(tun->recvQueue, data, numbytes);
+    if (status != B_OK)
+    {
+        return status;
     }
+    release_sem(*(tun->sendSem));
     return B_OK;
 }
 
-
-net_buffer*
-create_filled_buffer(uint8* data, size_t bytes)
-{
-	net_buffer* buffer = gBufferModule->create(256);
-	if (buffer == NULL) {
-		dprintf("creating a buffer failed!\n");
-		return NULL;
-	}
-
-	status_t status = gBufferModule->append(buffer, data, bytes);
-	if (status != B_OK) {
-		dprintf("appending %lu bytes to buffer %p failed: %s\n", bytes, buffer,
-			strerror(status));
-		gBufferModule->free(buffer);
-		return NULL;
-	}
-	
-	return buffer;
-}
-
-
+/**
+ * @brief Write data to the TUN device.
+ *
+ * This function writes data to the TUN device specified by the given cookie.
+ *
+ * @param cookie   Pointer to the TUN device data (tun_struct).
+ * @param position The offset position for writing (not used in this implementation).
+ * @param data     Pointer to the data to be written.
+ * @param numbytes Pointer to the size of the data to be written. On output, it will
+ *                 contain the actual number of bytes written.
+ * @return status_t Returns B_OK if the write operation is successful,
+ *                 or an appropriate error code if there is a failure.
+ */
 status_t
 tun_write(void *cookie, off_t position, const void *data, size_t *numbytes)
 {
-    // Write data to driver 
-    dprintf("tun:write_driver: writting %s with length of %li bytes\n", (char*)data, *numbytes);
-    if ((strcmp((char*)cookie, "app") == 0) && (interfaceQ.Used() < 3000)) {
-        dprintf("Appending to interfaceQ\n");
-        interfaceQ.Add(create_filled_buffer((uint8 *)data, *numbytes));
-        release_sem(intSem);
-    } else if ((strcmp((char*)cookie, "tun") == 0) && (appQ.Used() < 3000)) {
-        dprintf("%s appending to appQ\n", (char*)cookie);
-        appQ.Add(create_filled_buffer((uint8 *)data, *numbytes));
-        release_sem(appSem);
-    } else { 
+    tun_struct *tun = static_cast<tun_struct *>(cookie);
+    size_t used = tun->sendQueue->Used();
+    status_t status;
+    status = acquire_sem_etc(tun->selfSendSem, 1, B_CAN_INTERRUPT, 0);
+    if (status < B_OK)
+    {
+        dprintf("Could not acquire sending sem\n");
+        *numbytes = 0;
+        return status;
+    }
+    if ((used + *numbytes) >= 30000)
+    {
+        dprintf("%li bytes used\n", used + *numbytes);
         return B_ERROR;
     }
+    while (!*(tun->recvSem))
+    {
+        snooze(10000);
+    }
+    tun->sendQueue->Add(create_filled_buffer((uint8 *)data, *numbytes));
+    release_sem(*(tun->recvSem));
     return B_OK;
 }
-
 
 status_t
 tun_readv(void *cookie, off_t position, const iovec *vec, size_t count, size_t *numBytes)
@@ -320,24 +384,22 @@ tun_readv(void *cookie, off_t position, const iovec *vec, size_t count, size_t *
     return EOPNOTSUPP;
 }
 
-
 status_t
 tun_writev(void *cookie, off_t position, const iovec *vec, size_t count, size_t *numBytes)
 {
     return EOPNOTSUPP;
 }
 
-
-const char**
+const char **
 publish_devices()
 {
+    /* Publish driver names to devfs */
     return device_names;
 }
 
-
-device_hooks*
+device_hooks *
 find_device(const char *name)
 {
+    /* Find hooks for driver functions */
     return &tun_hooks;
 }
-
