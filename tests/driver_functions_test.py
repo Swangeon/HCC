6f100e17df54f39aae5cd42fc18c2d90accafbcb
Sean Brady <60947265+Swangeon@users.noreply.github.com>
2023-08-30 15:09:22 -0500

    Update driver_functions_test.py

--- tests/driver_functions_test.py
+++ tests/driver_functions_test.py
@@ -1,8 +1,31 @@
-file = open("/dev/misc/tun_driver", "rb+")
+import unittest
+import select
 
-input("Press enter to continue to write")
-file.write(b"0000000000000000000000000800450000549ca40000fe01f7dc0a0a0a0a0a0a0a0a0800e80357020019039856486efd050008090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637")
-input("Press enter to continue to read")
-file.read(1)
+class TestDriver(unittest.TestCase):
+    def setUp(self):
+        self.driver = open("/dev/tap0")
 
-file.close()
+    def test_read_from_driver(self):
+        # Mock some data in the driver
+        self.driver.write(b"test data")
+
+        # Use select to check for readability
+        rlist, _, _ = select.select([self.driver], [], [], 1)
+        self.assertTrue(self.driver in rlist)
+
+        # Read from the driver
+        data = self.driver.read(9)
+        self.assertEqual(data, b"test data")
+
+    def test_write_to_driver(self):
+        # Use select to check for writability
+        _, wlist, _ = select.select([], [self.driver], [], 1)
+        self.assertTrue(self.driver in wlist)
+
+        # Write to the driver
+        nbytes = self.driver.write(b"write test")
+        self.assertEqual(nbytes, 10)
+        self.assertEqual(self.driver.data, b"write test")
+
+if __name__ == "__main__":
+    unittest.main()
